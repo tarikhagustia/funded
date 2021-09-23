e93a55cef71518c3b4e1630f87f0c52ae06f51ac
Tarikh Agustia <agustia.tarikh150@gmail.com>
2021-09-23 11:11:31 +0700

    Bug Fix

--- app/Http/Livewire/Console/Dashboard/TradingStatistic.php
+++ app/Http/Livewire/Console/Dashboard/TradingStatistic.php
@@ -35,7 +35,7 @@ class TradingStatistic extends Component
 
     public function initialLoading()
     {
-        // $this->fetchSymbol();
+        $this->fetchSymbol();
 
         $this->fetchOrders();
         //
