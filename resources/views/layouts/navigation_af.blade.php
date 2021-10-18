2fae908b8a043d65778eab14af345195672efc87
Tarikh Agustia <agustia.tarikh150@gmail.com>
2021-10-18 15:05:07 +0700

    Level on group

--- resources/views/layouts/navigation_af.blade.php
+++ resources/views/layouts/navigation_af.blade.php
@@ -26,16 +26,17 @@
                 <li class="c-sidebar-nav-item"><a class="c-sidebar-nav-link" href="{{ route('comm.realtime') }}">{{ __('Commission') }}</a></li>
             </ul>
         </li>
-
-        <li class="c-sidebar-nav-item c-sidebar-nav-dropdown"><a
-                    class="c-sidebar-nav-link c-sidebar-nav-dropdown-toggle" href="#">
-                <i class="c-sidebar-nav-icon fas fa-file-invoice-dollar"></i>
-                {{ __('Operating costs') }}</a>
-            <ul class="c-sidebar-nav-dropdown-items">
-                <li class="c-sidebar-nav-item"><a class="c-sidebar-nav-link" href="{{ route('costs-operation.approval') }}">{{ __('Costs Approval') }}</a></li>
-                <li class="c-sidebar-nav-item"><a class="c-sidebar-nav-link" href="{{ route('costs-operation.request') }}">{{ __('Costs Request') }}</a></li>
-            </ul>
-        </li>
+        @if(auth()->user()->level_on_group == 4 || (auth()->user()->parent && auth()->user()->parent->level_on_group == 4))
+            <li class="c-sidebar-nav-item c-sidebar-nav-dropdown"><a
+                        class="c-sidebar-nav-link c-sidebar-nav-dropdown-toggle" href="#">
+                    <i class="c-sidebar-nav-icon fas fa-file-invoice-dollar"></i>
+                    {{ __('Operating costs') }}</a>
+                <ul class="c-sidebar-nav-dropdown-items">
+                    <li class="c-sidebar-nav-item"><a class="c-sidebar-nav-link" href="{{ route('costs-operation.approval') }}">{{ __('Costs Approval') }}</a></li>
+                    <li class="c-sidebar-nav-item"><a class="c-sidebar-nav-link" href="{{ route('costs-operation.request') }}">{{ __('Costs Request') }}</a></li>
+                </ul>
+            </li>
+        @endif
 
 {{--        <li class="c-sidebar-nav-item c-sidebar-nav-dropdown"><a--}}
 {{--                    class="c-sidebar-nav-link c-sidebar-nav-dropdown-toggle" href="#">--}}
